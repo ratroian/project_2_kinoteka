32e0bc20096a5b6dd1aea6075057ad04c17c52f1
duhenetsaleksandr <duhenets@gmail.com>
2021-11-27 09:49:55 +0200

    change way to loader img from html

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
 const ESLintPlugin = require('eslint-webpack-plugin');
 
 module.exports = {
@@ -32,22 +31,14 @@ module.exports = {
             favicon: "./favicon.png"
         }),
         new CleanWebpackPlugin(),
-        // new CopyWebpackPlugin({
-        //     patterns: [
-        //         {
-        //             from: path.resolve(__dirname, 'src/favicon.png'),
-        //             to: path.resolve(__dirname, 'dist'),
-        //         },
-        //         {
-        //             from: path.resolve(__dirname, 'src/images/Kinoteka-logo.svg'),
-        //             to: path.resolve(__dirname, 'dist/images'),
-        //         }
-        //     ]
-        // }),
         new ESLintPlugin(),
     ],
     module: {
         rules: [
+            {
+                test: /\.html$/i,
+                loader: "html-loader",
+            },
             {
                 test: /\.css$/i,
                 use: ["style-loader", "css-loader"],
