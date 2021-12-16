ab753ec68177742c5f7bca27513c4cac629b23ff
Natalia Huberna <natalyahuberna@gmail.com>
2021-12-16 23:15:01 +0200

    movie is ready

--- src/scripts/constants.js
+++ src/scripts/constants.js
@@ -1,6 +1,7 @@
 export const URL_IMG = 'https://image.tmdb.org/t/p/original/';
 export const API_URL = 'https://wowmeup.pp.ua';
 export const URL_MOVIE = `${API_URL}/movie`;
+export const URL_GENRES = `${API_URL}/genres`;
 
 export const MOVIE_ON_PAGE = 8;
 export const PER_PAGE = 40;
--- src/scripts/movies/global-var.js
+++ src/scripts/movies/global-var.js
@@ -2,11 +2,9 @@ import { getCurrentPageFromApi } from './helpers';
 
 export const domElements = {
     filmItemTemplate: document.querySelector('#filmItemTemplate').innerHTML,
-    // movieTemplate: document.querySelector('#movieTemplate').innerHTML,
     movieList: document.querySelector('#movie-list'),
     loadMoreBtn: document.querySelector('#load-more'),
     logOutBtn: document.querySelector('#log-out'),
-    // movieWrapper: document.querySelector('.movie-wrapper'),
 };
 
 export const globalVar = {
