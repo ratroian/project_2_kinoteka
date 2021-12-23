dfe644c4189752e2d585e825e22c29fb06284a8c
ratroian <300493rt@gmail.com>
2021-12-23 21:00:47 +0200

    fixed start values

--- src/scripts/filters/filters.js
+++ src/scripts/filters/filters.js
@@ -1,10 +1,7 @@
-// import axios from 'axios';
 import axios from 'axios';
 import { URL_MOVIE } from '../constants';
 import * as render from '../movies/render-movies';
-// import { globalVar } from '../movies/global-var';
 import * as helpers from '../movies/helpers';
-// import { getMoviesFromAPI } from '../movies/requests';
 
 const form = document.querySelector('#filters');
 const formItems = form.querySelectorAll('[data-url]');
@@ -16,14 +13,12 @@ const getUrl = () => {
             result += `&${element.dataset.url}=${element.value.replace(/,/g, '').replace(/ /g, '%20')}`;
         }
     });
-    console.log(result);
     return result;
 };
 
 const getMoviesFromAPIFilter = async (url) => {
     try {
         const response = await axios.get(url);
-        console.log(response.data);
         return response.data;
     } catch (error) {
         return [];
@@ -33,19 +28,17 @@ const getMoviesFromAPIFilter = async (url) => {
 const getMovies = async () => {
     try {
         render.showLoader();
-        const page = await getMoviesFromAPIFilter('https://wowmeup.pp.ua/movie?popularity_min=0&popularity_max=1000&&budget_min=0&budget_max=800000000&revenue_min=0&revenue_max=200000&title=Hope');
+        const page = await getMoviesFromAPIFilter(getUrl());
         if (page.movies.length === 0) throw new Error();
         helpers.saveMovies(page.movies);
     } finally {
         render.hideLoader();
     }
 };
-getMovies();
-console.log(getMovies());
 
 const formEventHandler = async (event) => {
     event.preventDefault();
-    getUrl();
+    getMovies();
 };
 
 form.addEventListener('submit', formEventHandler);
